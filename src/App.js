af834efb91e0bc5a711a2c72d97dabc79dc47ac6
Sully's Mac <sully@Sullyss-MacBook-Pro.local>
2021-11-30 17:16:14 -0700

    cards showing in browser, start search feature

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import Card from "./components/Card/Card";
 import Filter from "./components/Filter/Filter";
+import Search from "./components/Search/Search";
 
 function App() {
   let [fetchData, updateFetchData] = useState([]);
+  let [pageNumber, updatePageNumber] = useState(1);
+  let [search, setSearch] = useState("");
   let { info, results } = fetchData;
-  let api = `https://rickandmortyapi.com/api/character/?page=1`;
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}`;
   useEffect(() => {
     (async function () {
       let data = await fetch(api).then((res) => res.json());
@@ -19,6 +22,7 @@ function App() {
   return (
     <div className="App">
       <h1 className="text-center mb-3">Characters</h1>
+      <Search setSearch={setSearch} updatePageNumber={updatePageNumber} />
       <div className="container">
         <div className="row">
           <Filter />
