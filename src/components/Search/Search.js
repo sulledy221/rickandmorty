e5d52d8d16fa3f4ad69c55984fb2d51690475318
Sully's Mac <sully@Sullyss-MacBook-Pro.local>
2021-11-30 17:28:08 -0700

    Search bar component set up

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,5 +1,29 @@
 import React from "react";
+import styles from "./Search.module.scss";
 
-export default function Search() {
-  return <div></div>;
+export default function Search({ setSearch, updatePageNumber }) {
+  let searchBtn = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <form
+      className={`${styles.search} d-flex flex-sm-row flex-column align-items-center justify-content-center gap-4 mb-5`}
+    >
+      <input
+        onChange={(e) => {
+          updatePageNumber(1);
+          setSearch(e.target.value);
+        }}
+        placeholder="Thanks, Mr. Poopybutthole. I always could count on you."
+        className={styles.input}
+        type="text"
+      />
+      <button
+        onClick={searchBtn}
+        className={`${styles.btn} btn btn-primary fs-5`}
+      >
+        Wubba lubba dub dub!
+      </button>
+    </form>
+  );
 }
